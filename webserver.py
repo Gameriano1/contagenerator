573e0efaf36be15693a8327c9ab7f68f21a67901
leleodocapa <75039467+Gameriano1@users.noreply.github.com>
2024-10-24 23:37:03 -0300

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -6,14 +6,14 @@ app = Flask('')
 @app.route("/")
 def home():
   global aleatorio
-  with open("users.txt") as f:
+  with open("contas.txt") as f:
     liness = [line.rstrip('\n') for line in f]
   aleatorio = str(random.choice(liness))
   return render_template("home.html", conta=aleatorio)
 
 @app.route("/deletar_conta/", methods=['POST'])
 def deletarconta():
-  with open("users.txt", "r") as w:
+  with open("contas.txt", "r") as w:
     lines =  w.readlines()
   with open("users.txt", "w") as w:
     for line in lines:
